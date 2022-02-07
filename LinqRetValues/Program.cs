483b8fb6d209fb50564abc8b87d7477a800c5753
GreedNeSS <ruslan199126@gmail.com>
2022-02-07 20:09:09 +0300

    Methods return IEnumerable<T> and Array

---
+++ LinqRetValues/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LinqRetValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** GetStringSubset() *****");
+
+            IEnumerable<string> redColors1 = GetStringSubset();
+
+            foreach (string item in redColors1)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\n***** GetStringSubsetAsArray() *****");
+
+            Array redColors2 = GetStringSubsetAsArray();
+
+            foreach (string item in redColors2)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static IEnumerable<string> GetStringSubset()
+        {
+            string[] colors = { "Red", "Light Red", "Green", "Yellow", "Dark Red", "Purple" };
+
+            IEnumerable<string> theRedColors = from c in colors
+                                               where c.Contains("Red")
+                                               select c;
+
+            return theRedColors;
+        }
+
+        static Array GetStringSubsetAsArray()
+        {
+            string[] colors = { "Red", "Light Red", "Green", "Yellow", "Dark Red", "Purple" };
+
+            var theRedColors = (from c in colors
+                               where c.Contains("Red")
+                               select c).ToArray();
+
+            return theRedColors;
+        }
+    }
+}
