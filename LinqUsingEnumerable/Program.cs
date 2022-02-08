286aebe7dbbce814330c620d4721c9093d0afffb
GreedNeSS <ruslan199126@gmail.com>
2022-02-08 19:50:38 +0300

    Using Enumerable Methods

---
+++ LinqUsingEnumerable/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqUsingEnumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] currentVideoGames = {"Morrowind", "Uncharted 2",
+                "Fallout 3", "Daxter", "System Shock 2"};
+
+            QueryStringWithOperators(currentVideoGames);
+            Console.WriteLine();
+
+            QueryStringWithEnumerableAndLambdas(currentVideoGames);
+            Console.WriteLine();
+
+            QueryStringWithEnumerableAndLambdas2(currentVideoGames);
+            Console.WriteLine();
+
+            QueryStringsWithAnonymousMethods(currentVideoGames);
+            Console.WriteLine();
+
+            VeryComplexQueryExpression.QueryStringsWithRawDelegates(currentVideoGames);
+            Console.WriteLine();
+        }
+
+        static void QueryStringWithOperators(string[] arr)
+        {
+            Console.WriteLine("=> QueryStringWithOperators()");
+
+            var subset = from game in arr
+                         where game.Contains(" ")
+                         orderby game
+                         select game;
+
+            foreach (var game in subset)
+            {
+                Console.WriteLine($"Item: {game}");
+            }
+        }
+
+        static void QueryStringWithEnumerableAndLambdas(string[] arr)
+        {
+            Console.WriteLine("=> QueryStringWithEnumerableAndLambdas()");
+
+            var subset = arr.Where(g => g.Contains(" ")).OrderBy(i => i).Select(item => item);
+
+            foreach (var game in subset)
+            {
+                Console.WriteLine($"Item: {game}");
+            }
+        }
+
+        static void QueryStringWithEnumerableAndLambdas2(string[] arr)
+        {
+            Console.WriteLine("=> QueryStringWithEnumerableAndLambdas2()");
+
+            var gamesWithSpaces = arr.Where(game => game.Contains(" "));
+            var orderedGames = gamesWithSpaces.OrderBy(game => game);
+            var subset = orderedGames.Select(game => game);
+
+            foreach (var game in subset)
+            {
+                Console.WriteLine($"Item3: {game}");
+            }
+        }
+
+        static void QueryStringsWithAnonymousMethods(string[] arr)
+        {
+            Console.WriteLine("=> QueryStringsWithAnonymousMethods()");
+
+            Func<string, bool> searchFilter = delegate (string str)
+            {
+                return str.Contains(" ");
+            };
+
+            Func<string, string> itemToProcess = delegate (string str)
+            {
+                return str;
+            };
+
+            var subset = arr.Where(searchFilter).OrderBy(itemToProcess).Select(itemToProcess);
+
+            foreach (var game in subset)
+            {
+                Console.WriteLine($"Item: {game}");
+            }
+        }
+    }
+}
---
+++ LinqUsingEnumerable/VeryComplexQueryExpression.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqUsingEnumerable
+{
+    class VeryComplexQueryExpression
+    {
+        public static void QueryStringsWithRawDelegates(string[] arr)
+        {
+            Console.WriteLine("=> VeryComplexQueryExpression.QueryStringsWithRawDelegates()");
+
+            Func<string, bool> searchFilter = new Func<string, bool>(Filter);
+            Func<string, string> itemToProcess = new Func<string, string>(ToProcess);
+
+            var subset = arr.Where(searchFilter).OrderBy(itemToProcess).Select(itemToProcess);
+
+            foreach (var game in subset)
+            {
+                Console.WriteLine($"Item: {game}");
+            }
+        }
+
+        static bool Filter(string str)
+        {
+            return str.Contains(" ");
+        }
+
+        static string ToProcess(string str)
+        {
+            return str;
+        }
+    }
+}
